41cdd987d39cbd3994def001eb99192185447ec7
Bekarys <230103246@sdu.edu.kz>
2025-09-24 00:32:47 +0500

    i added lombok

--- src/main/java/com/Tanda/entity/User.java
+++ src/main/java/com/Tanda/entity/User.java
@@ -8,20 +8,27 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
 @Entity
 @Table(
         name = "users",
         uniqueConstraints = {
-                @UniqueConstraint(
-                        name = "username_unique",
-                        columnNames = "username"
-                ),
-                @UniqueConstraint(
-                        name = "email_unique",
-                        columnNames = "email"
-                )
-
-})
+                @UniqueConstraint(name = "username_unique", columnNames = "username"),
+                @UniqueConstraint(name = "email_unique", columnNames = "email")
+        }
+)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User implements UserDetails {
 
     @Id
@@ -29,39 +36,22 @@ public class User implements UserDetails {
     @Column(name = "id")
     private Long id;
 
-    @Column(
-            name = "first_name",
-            nullable = false
-    )
+    @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @Column(
-            name = "last_name",
-            nullable = false
-    )
+    @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @Column(
-            name = "username",
-            unique = true
-    )
+    @Column(name = "username", unique = true)
     private String username;
 
-    @Column(
-            name = "password",
-            nullable = false
-    )
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(
-            name = "email",
-            unique = true
-    )
+    @Column(name = "email", unique = true)
     private String email;
 
-    @Column(
-            name = "enabled"
-    )
+    @Column(name = "enabled")
     private boolean enabled;
 
     @Column(name = "locked")
@@ -70,21 +60,9 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
     }
 
     @Override
@@ -106,101 +84,5 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return enabled;
     }
-
-    public User() {
-    }
-
-    public User(
-            String firstName,
-            String lastName,
-            String username,
-            String password,
-            String email,
-            boolean enabled,
-            boolean locked,
-            Role role) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.enabled = enabled;
-        this.locked = locked;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public boolean isLocked() {
-        return locked;
-    }
-
-    public void setLocked(boolean locked) {
-        this.locked = locked;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", enabled=" + enabled +
-                ", locked=" + locked +
-                ", role=" + role +
-                '}';
-    }
 }
+
